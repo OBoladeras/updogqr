306fbd5575c7b3b59c3d736c6191e497ad95b501
Boladeras <127840531+OBoladeras@users.noreply.github.com>
2024-06-26 17:31:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='updogqr',
-    version='0.26',
+    version='0.3',
     packages=find_packages(),
     install_requires=[
         'qrcode',
